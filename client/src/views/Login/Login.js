9c2cb15c110b5ae85eb271d79489043133a81f04
Sinadin Shan <sinadinshan66@gmail.com>
2021-11-07 13:13:36 +0530

    Styled login page, improved submit

--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -10,11 +10,14 @@ const Login = (props) => {
 
     const dispatch = useDispatch()
 
-    async function onSubmitClick()
+    async function onFormSubmit(event)
     {
+        event.preventDefault()
         const email = document.getElementById('emailInput').value
         const password = document.getElementById('passwordInput').value
 
+        console.log(email,password)
+        
         const requestOptions = 
         {
             method: 'POST',
@@ -32,27 +35,54 @@ const Login = (props) => {
             localStorage.setItem('token', data['access_token'])
             props.validateToken(data['access_token'])
         }
+        else{
+            alert("Invalid login credentials")
+        }
     }
 
     return (
-        <div className="loginFormWrapper">
+        // <div className="loginFormWrapper">
+        //     <Form>
+        //         <Form.Group className="mb-3" controlId="formBasicEmail">
+        //             <Form.Label>Email</Form.Label>
+        //             <Form.Control type="email" id="emailInput" placeholder="Enter email" />
+        //         </Form.Group>
 
-        
-            <Form>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" id="emailInput" placeholder="Enter email" />
-                </Form.Group>
+        //         <Form.Group className="mb-3" controlId="formBasicPassword">
+        //             <Form.Label>Password</Form.Label>
+        //             <Form.Control type="password" id="passwordInput" placeholder="Password" />
+        //         </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" id="passwordInput" placeholder="Password" />
-                </Form.Group>
+        //         <Button id="submitBtn" variant="primary" type="button" onClick={onSubmitClick}>
+        //             Submit
+        //         </Button>
+        //     </Form>
+        // </div>
 
-                <Button id="submitBtn" variant="primary" type="button" onClick={onSubmitClick}>
-                    Submit
-                </Button>
-            </Form>
+        <div class="container">
+            <div class="row justify-content-center d-flex flex-column min-vh-100 align-items-center">
+                <div class="col-9 col-sm-8 col-lg-6 contact-form justify-content-center">
+                    <h2 id='heading'>Login</h2>
+                    <div class="row justify-content-center text-center">
+                        <form onSubmit={onFormSubmit}>
+                            <div class="form-group form-floating">
+                                <input type="text" required name="username" class="form-control username" placeholder="email" id="emailInput"/>
+                                <label for="email">Email</label>
+                            </div>
+                            <div class="form-group form-floating">
+                                <input type="password" required name="password" class="form-control password" id="passwordInput" placeholder='password'/>
+                                <label for="password">Password</label>
+                            </div>
+
+                            <span>
+                                <Button id="submitBtn" variant="primary" type="submit">
+                                    Submit
+                                </Button>
+                            </span>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
