2a4fddf93557587fa9472a63659106c20713388a
Sinadin Shan <sinadinshan66@gmail.com>
2022-10-28 10:14:14 +0530

    Make function for formatting db result

--- node_server/db.js
+++ node_server/db.js
@@ -1,7 +1,17 @@
 // set up db and start connection
 const {Client} = require('pg')
 
-let db = {};
+function format_result(db_result){
+  let result = []
+  for(row of db_result.rows){
+    let entry = []
+    for(key in row){
+      entry.push(row[key])
+    }
+    result.push(entry)
+  }
+  return result
+}
 
 async function getMesses(){
 
@@ -13,21 +23,17 @@ async function getMesses(){
     await client.connect()
     const messes = await client.query('select mess_id, messname from mess where mess_id in (select mess_id from managers)')
 
-    // convert db result to former python-backend format
-    // here result obtained is of the form {.."rows":[{"..":".."},{"..":".."}]..} 
-    let result = []
-    for(row of messes.rows){
-      let entry = []
-      for(key in row){
-        entry.push(row[key])
-      }
-      result.push(entry)
-    }
+    // Here result obtained is of the form {.."rows":[{"..":".."},{"..":".."}]..} 
+    // so, convert db result to former python-backend format [..(,)..]
+    let result = format_result(messes)
 
     await client.end()
     return result
 }
 
+let db = {};
+
+// Add all functions to property of object db
 db.getMesses = getMesses
 
 module.exports = db
